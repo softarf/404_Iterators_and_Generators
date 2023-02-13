243c121e8eb92b122741004231f30957d4475e02
softarf <98207887+softarf@users.noreply.github.com>
2023-02-13 23:47:50 +0500

    Аннотирует переменные, вносит косметические исправления. ДЗ 4, задача 1.

--- task_4_4_1_Iterators.py
+++ task_4_4_1_Iterators.py
@@ -1,37 +1,44 @@
 # -*- coding: utf-8 -*-
+#                       Итераторы и генераторы.
+#       Задача 1. Получить итератор, который принимает список списков и возвращает его плоское представление.
+
+from typing import Any, List
+
 
 class FlatIterator:
 
-    def __init__(self, list_of_list):
-        self.list_of_list = list_of_list
-        self.index = -1
+    def __init__(self, list_of_list: List[List[Any]]) -> None:
+        """Создаёт объект обрабатываемого списка."""
+        self.list_of_list: List[List[Any]] = list_of_list
+        self.index: int
+        self.next_list: List[Any] = []
 
-    def __iter__(self):
+    def __iter__(self) -> Any:
+        """Подготавливает список для обработки в цикле."""
+        self.index = -1
         self.next_list = []
         return self
 
-    def __next__(self):
-        # print(self.index, self.list_of_list)
+    def __next__(self) -> Any:
+        """Выполняет одну итерацию из цикла, возвращает одно значение из списка."""
         if not self.next_list:
-            # next_nested_list iterated_list - Вложенный и Итерируемый, Следующий и (previous) Предыдущий
             self.index += 1
-            if self.index == len(self.list_of_list):
+            if self.index >= len(self.list_of_list):
                 raise StopIteration
-            # self.next_list = list(self.list_of_list[self.index])
-            self.next_list = self.list_of_list[self.index][:]
-            print(type(self.list_of_list[self.index]), isinstance(self.list_of_list[self.index], list))
-            print(self.index, self.next_list)
-        return self.next_list.pop(0)  # IndexError: pop from empty list - "pop из пустого списка"
+            # self.next_list = list(self.list_of_list[self.index])    # 1) Тоже работает.
+            self.next_list = self.list_of_list[self.index][:]  # 2)
+        return self.next_list.pop(0)
 
 
 def test_1():
-    list_of_lists_1 = [
+    list_of_lists_1: List[List[Any]] = [
         ['a', 'b', 'c'],
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
 
     print(f'\nlist_of_lists_1: {list_of_lists_1}\n')
+
     for flat_iterator_item, check_item in zip(
             FlatIterator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
@@ -39,7 +46,7 @@ def test_1():
         print(flat_iterator_item, check_item)
         assert flat_iterator_item == check_item
 
-    print(f'\nlist_of_lists_1: {list_of_lists_1}\n')
+    print(f'\nlist_of_lists_1: {list_of_lists_1}')
 
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
